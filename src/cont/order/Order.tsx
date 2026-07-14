4bf82df7bde71e8f8c2d696fa0b8ee72e38f27b6
ㅇㅅㅇ <rlawlsrb4011@naver.com>
2026-07-14 10:36:23 +0900

    order

--- src/cont/order/Order.tsx
+++ src/cont/order/Order.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Stayle from './order.module.css'
 import MyCanvasPage from './Signature'
 const Order: React.FC = () => {
-  
-  
+
+
   return (
     <div>
       {/* <div className={Stayle.header_container}>
@@ -12,50 +12,58 @@ const Order: React.FC = () => {
                 <button className={Stayle.header_container_btn}>test1</button>
             </div>
             <hr/> */}
-      <h2>발주자 작성란</h2>
+      <div className={Stayle.header_container}>
+        <h2 className={Stayle.header_container_text_right}>발주자 작성란</h2>
+        <h3 className={Stayle.div_text}>서명 및 발주하기</h3>
+      </div>
       <form>
         <div className={Stayle.header_container}>
-        <div className={Stayle.header_container_text_right}>
-          <ul className={Stayle.header_container_li}>
+          <div className={Stayle.header_container_text_right}>
+            <ul className={Stayle.header_container_li}>
+              <li>발주일 : <input type="text" name='date' /></li>
+              <li>발주일 : <input type="text" name='date' /></li>
+              <li>발주일 : <input type="text" name='date' /></li>
+              <li>발주일 : <input type="text" name='date' /></li>
+              <li>발주일 : <input type="text" name='date' /></li>
+            </ul>
+          </div>
+          <div className={Stayle.header_container_text_left}>
+            {/* <ul>
             <li>발주일 : <input type="text" name='date' /></li>
             <li>발주일 : <input type="text" name='date' /></li>
             <li>발주일 : <input type="text" name='date' /></li>
             <li>발주일 : <input type="text" name='date' /></li>
             <li>발주일 : <input type="text" name='date' /></li>
-          </ul>
+          </ul> */}
+
+            <MyCanvasPage />
+            {/* <input type="button" name='date'>발주하기</input> */}
+          </div>
         </div>
-        <div className={Stayle.header_container_text_left}>
-          <ul>
-            <li>발주일 : <input type="text" name='date' /></li>
-            <li>발주일 : <input type="text" name='date' /></li>
-            <li>발주일 : <input type="text" name='date' /></li>
-            <li>발주일 : <input type="text" name='date' /></li>
-            <li>발주일 : <input type="text" name='date' /></li>
-          </ul>
-          {/* <input type="button" name='date'>발주하기</input> */}
-        </div>
-        </div>
-        <MyCanvasPage/>
+
+
+
+        <table className={Stayle.Table}>
+          <thead>
+            <tr>
+              <th>도서명</th>
+              <th>출판사</th>
+              <th>단가</th>
+              <th>합계금액</th>
+              <th>수량</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th><input type="text"></input></th>
+              <th><input type="text"></input></th>
+              <th><input type="text"></input></th>
+              <th>tsx영역, 단가x수량만큼의 값을 출력</th>
+              <th><input type="text"></input></th>
+            </tr>
+          </tbody>
+        </table>
       </form>
-      
-      <table className={Stayle.Table}>
-        <thead>
-          <tr>
-            <th>뭐시기</th>
-            <th>뭐시기</th>
-            <th>뭐시기</th>
-            <th>뭐시기</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th>더미데이터</th>
-            <th>더미데이터</th>
-            <th>더미데이터</th>
-            <th>더미데이터</th>
-          </tr>
-        </tbody>
-      </table>
     </div>
   )
 }
