59fb65061373fc0e4f659c8bc92900796c6c0b62
ㅇㅅㅇ <rlawlsrb4011@naver.com>
2026-07-24 09:28:11 +0900

    조원들을 위한 주석 달아놨습니다 / 프로덕트

--- src/cont/product/Product.tsx
+++ src/cont/product/Product.tsx
@@ -18,10 +18,13 @@ interface ProductItem {
     piunitCost: number; //출판사
 }
 
+
+//백엔드 주소가 담긴 .env파일의 키값
 const backendUrl = process.env.REACT_APP_BACK_END_URL;
 
 const Product: React.FC = () => {
 
+    //useState 
     //--------form 
     const [productForm, setProductForm] = useState<ProductForm | null>(null);
 
@@ -37,38 +40,53 @@ const Product: React.FC = () => {
     const [piunitCost, setpiunitCost] = useState<number>(0);
     const pisumprice = (piprice && piamount) ? (piprice * piamount) : 0;
 
-    //--------추가로 필요한 훅들
+    //useNavigate
     const nav = useNavigate();
 
-
-    //-----ts
+    // 트리거함수
     const productSumit = async () => {
+        
+        //날짜를 선택하지 않고 제출한 케이스에 대해 예외처리
         if (!pstartdate || !penddate) {
             alert("시작일과 마감일을 모두 선택해 주세요.");
             return;
         }
+        // 판매정보를 등록하지 않고 제출한 케이스에 대해 예외처리
         if (!productItem || productItem.length === 0) {
             alert("등록할 상품을 최소 하나 이상 추가해 주세요.");
             return;
         }
         
         const isConfirmed = window.confirm("수정이 불가합니다.\n정말 등록하시겠습니까?\n(수락시 매출관리 페이지로 이동됩니다.)");
+        
+        //경고창의 결과값이 수락일경우 실행
         if (isConfirmed) {
+            //백엔드에 보낼 데이터 조립
             const assembledForm = {
                 pstartdate: pstartdate,
                 penddate: penddate,
                 productItem: productItem,
             };
+            //혹시몰라서 useState setter깔아둡니다 이 부분은 지우셔도 무방합니다
             setProductForm(assembledForm);
             try {
+                // 서버에 보낼 데이터 json화
+                // 주의: productForm를 보내지않는 이유 
+                // useState는 비동기처리라 productForm를 보내고싶으면 어딘가에서 이 함수가 실행되기전에 
+                // productForm를 랜더링하는 과정을 일으켜야합니다
                 const productData = JSON.stringify(assembledForm);
+
+                //백엔드로 전송 (fetch 사용), pa)axios사용해 보실 분들은 저한테 말하고 바꾸셔도 됩니다!
                 const res = await fetch(`${backendUrl}/api/productForm`, {
                     method: 'POST',
+                    //header-Type은 application/json : 백엔드에 보낼 데이터가 순수한 json형태(포맷)이기 때문
                     headers: {
                         'Content-Type': 'application/json',
                     },
                     body: productData,
                 });
+
+                //전송결과 출력
                 if (res.ok) {
                     nav("/revenue");
                 } else {
@@ -103,14 +121,12 @@ const Product: React.FC = () => {
     return (
         <div className={Stayle.container}>
             <h2>판매 폼</h2>
-
             <div className={Stayle.sal_div_header}>
                 <p>시작일 : <input type='date' name='pstartdate' onChange={(e) => { setPstartdate(e.target.value) }} /></p>
                 <p> ~ </p>
                 <p>마감일 : <input type='date' name='penddate' onChange={(e) => { setPenddate(e.target.value) }} /></p>
             </div>
             <h2 className={Stayle.total_price_title}>총 판매 단가 : {pisumprice.toLocaleString()}원</h2>
-            {/* 입력 테이블 */}
             <table className={Stayle.Table}>
                 <thead>
                     <tr>
@@ -141,7 +157,6 @@ const Product: React.FC = () => {
                 </tfoot>
             </table>
             <hr />
-            {/* 등록된 리스트 테이블 */}
             <table className={Stayle.Table}>
                 <thead>
                     <tr>
